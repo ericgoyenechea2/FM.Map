6c417c71e397e2332d5d10400b511c5ca8cc198a
Victor <vfernandezmeca@gmail.com>
2023-09-06 09:02:13 +0200

    maps components

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import FilterView from '../views/FilterView.vue'
 import MapView from '../views/MapView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: FilterView
-    },
+   
     {
       path: '/map',
       name: 'map',
